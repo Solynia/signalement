7388f5aed8fcd87125f43afe94ffc4ae65cf84fd
Sebastien Bouvier <sebastien.bouvier@outlook.com>
2025-04-13 15:53:46 +0700

    chore: define lib dependency boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,56 @@ export default [
               sourceTag: '*',
               onlyDependOnLibsWithTags: ['*'],
             },
+            {
+              sourceTag: 'type:feature',
+              onlyDependOnLibsWithTags: [
+                'type:data-access',
+                'type:feature',
+                'type:ui',
+                'type:utils',
+              ],
+            },
+            {
+              sourceTag: 'type:data-access',
+              onlyDependOnLibsWithTags: ['type:data-access', 'type:utils'],
+            },
+            {
+              sourceTag: 'type:ui',
+              onlyDependOnLibsWithTags: ['type:ui', 'type:utils'],
+            },
+            {
+              sourceTag: 'type:utils',
+              onlyDependOnLibsWithTags: ['type:utils'],
+            },
+            {
+              sourceTag: 'scope:author',
+              onlyDependOnLibsWithTags: ['scope:author', 'scope:shared'],
+            },
+            {
+              sourceTag: 'scope:signalement',
+              onlyDependOnLibsWithTags: [
+                'scope:author',
+                'scope:observation',
+                'scope:signalement',
+                'scope:shared',
+              ],
+            },
+            {
+              sourceTag: 'scope:observation',
+              onlyDependOnLibsWithTags: ['scope:observation', 'scope:shared'],
+            },
+            {
+              sourceTag: 'scope:shared',
+              onlyDependOnLibsWithTags: ['scope:shared'],
+            },
+            {
+              sourceTag: 'app:nest',
+              onlyDependOnLibsWithTags: ['app:nest'],
+            },
+            {
+              sourceTag: 'app:ng',
+              onlyDependOnLibsWithTags: ['app:ng'],
+            },
           ],
         },
       ],
