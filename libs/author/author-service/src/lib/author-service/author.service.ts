6fba2db18f096d17782692cbfa331f37a7cdc971
Sebastien Bouvier <sebastien.bouvier@outlook.com>
2025-04-18 12:19:26 +0700

    fix: convert string of date to date  objects
    necessary until backend implements class validation and transformation

--- libs/author/author-service/src/lib/author-service/author.service.ts
+++ libs/author/author-service/src/lib/author-service/author.service.ts
@@ -1,27 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import { Author, AuthorCreate, AuthorUpdate } from './author.model';
 
 const API_URL = '/api/author';
+
+// TODO implement in backend
+const mapStringToDate = (el: Author): Author => ({
+  ...el,
+  birth_date: new Date(el.birth_date),
+});
 
 @Injectable({ providedIn: 'root' })
 export class AuthorService {
   constructor(private httpClient: HttpClient) {}
 
   findById(id: string) {
-    return this.httpClient.get<Author>(`${API_URL}/${id}`);
+    return this.httpClient
+      .get<Author>(`${API_URL}/${id}`)
+      .pipe(map(mapStringToDate));
   }
 
   findAll() {
-    return this.httpClient.get<Author[]>(`${API_URL}`);
+    return this.httpClient
+      .get<Author[]>(`${API_URL}`)
+      .pipe(map((v) => v.map(mapStringToDate)));
   }
 
   create(data: AuthorCreate) {
-    return this.httpClient.post<Author>(`${API_URL}`, data);
+    return this.httpClient
+      .post<Author>(`${API_URL}`, data)
+      .pipe(map(mapStringToDate));
   }
 
   update(id: string, data: AuthorUpdate) {
-    return this.httpClient.put<Author>(`${API_URL}/${id}`, data);
+    return this.httpClient
+      .put<Author>(`${API_URL}/${id}`, data)
+      .pipe(map(mapStringToDate));
   }
 
   delete(id: string) {
--- libs/signalement/signalement-service/src/lib/signalement.service.ts
+++ libs/signalement/signalement-service/src/lib/signalement.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import {
   Signalement,
   SignalementCreate,
@@ -8,24 +9,41 @@ import {
 
 const API_URL = '/api/signalement';
 
+// TODO implement in backend
+const mapStringToDate = (el: Signalement): Signalement => ({
+  ...el,
+  author: {
+    ...el.author,
+    birth_date: new Date(el.author.birth_date),
+  },
+});
+
 @Injectable({ providedIn: 'root' })
 export class SignalementService {
   constructor(private httpClient: HttpClient) {}
 
   findById(id: string) {
-    return this.httpClient.get<Signalement>(`${API_URL}/${id}`);
+    return this.httpClient
+      .get<Signalement>(`${API_URL}/${id}`)
+      .pipe(map(mapStringToDate));
   }
 
   findAll() {
-    return this.httpClient.get<Signalement[]>(`${API_URL}`);
+    return this.httpClient
+      .get<Signalement[]>(`${API_URL}`)
+      .pipe(map((v) => v.map(mapStringToDate)));
   }
 
   create(data: SignalementCreate) {
-    return this.httpClient.post<Signalement>(`${API_URL}`, data);
+    return this.httpClient
+      .post<Signalement>(`${API_URL}`, data)
+      .pipe(map(mapStringToDate));
   }
 
   update(id: string, data: SignalementUpdate) {
-    return this.httpClient.put<Signalement>(`${API_URL}/${id}`, data);
+    return this.httpClient
+      .put<Signalement>(`${API_URL}/${id}`, data)
+      .pipe(map(mapStringToDate));
   }
 
   delete(id: string) {
